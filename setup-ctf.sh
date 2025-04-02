7d297e8346ca3b395711cd694a6ef6a6fc31ae5f
k1sta <pedrokitagawa@hotmail.com.br>
2025-04-02 18:19:20 -0300

    fixing stuff

--- setup-ctf.sh
+++ setup-ctf.sh
@@ -3,13 +3,15 @@
 # Downloads
 pip3 install pycryptodome pwn sage
 wget "https://github.com/NationalSecurityAgency/ghidra/releases/download/Ghidra_11.3.1_build/ghidra_11.3.1_PUBLIC_20250219.zip" -O ghidra.zip
-wget "https://download.oracle.com/java/24/latest/jdk-24_linux-x64_bin.tar.gz" -o jdk.tar.gz
-wget "https://portswigger.net/burp/releases/community/latest" -o burp.sh
+wget "https://download.oracle.com/java/24/latest/jdk-24_linux-x64_bin.tar.gz"
+wget "https://portswigger.net/burp/releases/community/latest"
 
 # Decompress
 unzip ghidra.zip
-tar -xvzf jdk.tar.gz
+tar -xvzf jdk-24_linux-x64_bin.tar.gz
 
-echo $(pwd jdk-24) | ./ghidra_11.3.1_PUBLIC/ghidraRun
+./ghidra_11.3.1_PUBLIC/ghidraRun
+sleep 2
+echo "$(pwd jdk-24)/jdk-24"
 
-chmod +x ./burp.sh; ./burp.sh
+chmod +x ./latest; ./latest
