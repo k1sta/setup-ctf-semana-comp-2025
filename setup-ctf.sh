f5cb62624a99c3b9f2776e864cbb146001517e3b
Pedro <113566710+k1sta@users.noreply.github.com>
2025-04-02 19:13:18 -0300

    changing pwndbg version

--- setup-ctf.sh
+++ setup-ctf.sh
@@ -12,7 +12,7 @@ pip3 install pycryptodome pwn sage
 wget "https://github.com/NationalSecurityAgency/ghidra/releases/download/Ghidra_11.3.1_build/ghidra_11.3.1_PUBLIC_20250219.zip" -O ghidra.zip
 wget "https://download.oracle.com/java/24/latest/jdk-24_linux-x64_bin.tar.gz"
 wget "https://portswigger.net/burp/releases/download?product=community&version=2025.2.3&type=Linux" -O burp
-wget "https://github.com/pwndbg/pwndbg/releases/download/2025.02.19/pwndbg-lldb_2025.02.19_x86_64-portable.tar.xz" -O pwndbg.tar.xz
+wget "https://github.com/pwndbg/pwndbg/releases/download/2025.02.19/pwndbg_2025.02.19_x86_64-portable.tar.xz" -O pwndbg.tar.xz
 
 
 # Decompress
@@ -20,7 +20,7 @@ unzip ghidra.zip
 tar -xvzf jdk-24_linux-x64_bin.tar.gz
 tar -xvf pwndbg.tar.xz
 
-export PATH="$PATH:$(pwd ./ghidra_11.3.1_PUBLIC/ghidraRun):$(pwd ./pwndbg/bin)"
+export PATH="$PATH:$(pwd ./ghidra_11.3.1_PUBLIC/ghidraRun):$(pwd ./pwndbg/bin/pwndbg)"
 export JAVA_HOME="$(pwd jdk-24)/jdk-24"
 ./ghidra_11.3.1_PUBLIC/ghidraRun
 
