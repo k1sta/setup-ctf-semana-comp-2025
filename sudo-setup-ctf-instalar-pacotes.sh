e37f9cc1d4dd73017a5729e08113227bb236a01e
k1sta <pedrokitagawa@hotmail.com.br>
2025-04-03 00:53:29 -0300

    adding setup files in case we have sudo access

---
+++ sudo-setup-ctf-instalar-pacotes.sh
@@ -0,0 +1,39 @@
+#!/bin/bash
+
+# Changing wallpaper
+# 1. Save the original wallpaper
+# 2. Download the GRIS wallpaper
+# 3. Change wallpaper to the downloaded one
+
+# Changing shell (sh in 2025 wtf?)
+chsh -s $(which bash)
+
+# Creating directory
+mkdir -p $HOME/bin/programas-ctf
+cd $HOME/bin/programas-ctf
+
+# Downloads
+pip3 install pycryptodome pwn Pillow
+wget "https://github.com/NationalSecurityAgency/ghidra/releases/download/Ghidra_11.3.1_build/ghidra_11.3.1_PUBLIC_20250219.zip" -O ghidra.zip
+wget "https://download.oracle.com/java/24/latest/jdk-24_linux-x64_bin.tar.gz" -O jdk-24.tar.gz
+wget "https://portswigger.net/burp/releases/download?product=community&version=2025.2.3&type=Linux" -O burp
+wget "https://github.com/pwndbg/pwndbg/releases/download/2025.02.19/pwndbg_2025.02.19_x86_64-portable.tar.xz" -O pwndbg.tar.xz
+
+# Decompress
+unzip ghidra.zip
+tar -xvzf jdk-24.tar.gz
+tar -xvf pwndbg.tar.xz
+
+# Adding variables and paths on env variables
+export PATH="$PATH:$(realpath ./ghidra_11.3.1_PUBLIC/ghidraRun):$(realpath ./pwndbg/bin/pwndbg)"
+export JAVA_HOME="$(pwd jdk-24)/jdk-24"
+
+# Making the changes persistents
+echo -e "export PATH=$PATH\nexport JAVA_HOME=$JAVA_HOME" >> $HOME/.bashrc
+
+# Installing programs with apt
+sudo apt install hexedit nmap wireshark sleuthkit autopsy
+
+# Running apps that need UI configuration
+./ghidra_11.3.1_PUBLIC/ghidraRun
+chmod +x ./burp; ./burp
---
+++ sudo-setup-ctf-remover-pacotes.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+# Purging programs with apt
+sudo apt install hexedit nmap wireshark sleuthkit autopsy
