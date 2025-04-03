1f2636a6d84b940a11e1979abbb8450d3f74b454
Pedro <113566710+k1sta@users.noreply.github.com>
2025-04-02 22:39:20 -0300

    renaming jdk24 zip

--- setup-ctf.sh
+++ setup-ctf.sh
@@ -15,13 +15,13 @@ cd $HOME/bin/programas-ctf
 # Downloads
 pip3 install pycryptodome pwn sage
 wget "https://github.com/NationalSecurityAgency/ghidra/releases/download/Ghidra_11.3.1_build/ghidra_11.3.1_PUBLIC_20250219.zip" -O ghidra.zip
-wget "https://download.oracle.com/java/24/latest/jdk-24_linux-x64_bin.tar.gz"
+wget "https://download.oracle.com/java/24/latest/jdk-24_linux-x64_bin.tar.gz" -O jdk-24.tar.gz
 wget "https://portswigger.net/burp/releases/download?product=community&version=2025.2.3&type=Linux" -O burp
 wget "https://github.com/pwndbg/pwndbg/releases/download/2025.02.19/pwndbg_2025.02.19_x86_64-portable.tar.xz" -O pwndbg.tar.xz
 
 # Decompress
 unzip ghidra.zip
-tar -xvzf jdk-24_linux-x64_bin.tar.gz
+tar -xvzf jdk-24.tar.gz
 tar -xvf pwndbg.tar.xz
 
 # Adding variables and paths on env variables
