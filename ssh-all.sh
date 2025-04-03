bf4cebfd7a6721a2fd229c01dce3664cd6f447d8
k1sta <pedrokitagawa@hotmail.com.br>
2025-04-03 10:42:58 -0300

    changed every single fucking thing

--- ssh-all.sh
+++ ssh-all.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
 
 while IFS= read -r line; do
-    sshpass -p "convidado" scp -o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null ./sudo-setup-ctf-instalar-pacotes.sh convidado@146.164.41.$line:/home/convidado/Downloads/
-
-    echo "Executing script on $line..."
-    sshpass -p <senha-wks> ssh -o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null wks@146.164.41.$line '/home/convidado/Downloads/sudo-setup-ctf-instalar-pacotes.sh'
+    	sshpass -p "convidado" scp -o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null ./sudo-setup-ctf-instalar-pacotes.sh convidado@146.164.41.$line:/home/convidado/Downloads/
+	sshpass -p "convidado" scp -o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null ./setup-ctf.sh convidado@146.164.41.$line:/home/convidado/Downloads/
+    	sshpass -p "convidado" ssh -o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null convidado@146.164.41.$line '/home/convidado/Downloads/setup-ctf.sh'
+    
+    	sshpass -p <senha-wks> ssh -o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null wks@146.164.41.$line '/home/convidado/Downloads/sudo-setup-ctf-instalar-pacotes.sh'
 done < ips.txt
--- sudo-setup-ctf-instalar-pacotes.sh
+++ sudo-setup-ctf-instalar-pacotes.sh
@@ -1,48 +1,6 @@
 #!/bin/bash
 
-# Changing wallpaper
-# 1. Save the original wallpaper
-# 2. Download the GRIS wallpaper
-# 3. Change wallpaper to the downloaded one
-
-echo "Placing it on home"
-cd /home/convidado
-
-echo "Changing user"
-echo <senha-wks> | sudo su convidado
-
-echo "Creating directory"
-mkdir -p $HOME/bin/programas-ctf
-cd $HOME/bin/programas-ctf
-
-echo "Changing shell (sh in 2025 wtf?)"
-chsh -s $(which bash)
-
-echo "Downloads"
-python3 -m pip install pycryptodome pwn Pillow
-wget "https://github.com/NationalSecurityAgency/ghidra/releases/download/Ghidra_11.3.1_build/ghidra_11.3.1_PUBLIC_20250219.zip" -O ghidra.zip -q
-wget "https://download.oracle.com/java/24/latest/jdk-24_linux-x64_bin.tar.gz" -O jdk-24.tar.gz -q
-wget "https://portswigger.net/burp/releases/download?product=community&version=2025.2.3&type=Linux" -O burp -q
-wget "https://github.com/pwndbg/pwndbg/releases/download/2025.02.19/pwndbg_2025.02.19_x86_64-portable.tar.xz" -O pwndbg.tar.xz -q
-
-echo Decompress
-unzip ghidra.zip
-tar -xvzf jdk-24.tar.gz
-tar -xvf pwndbg.tar.xz
-
-echo "Adding variables"
-export PATH="/usr/bin:$PATH:$(realpath ./ghidra_11.3.1_PUBLIC/ghidraRun):$(realpath ./pwndbg/bin/pwndbg)"
-export JAVA_HOME="$(pwd jdk-24)/jdk-24"
-
-echo "Making the changes persistents"
-echo -e "export PATH=$PATH\nexport JAVA_HOME=$JAVA_HOME" >> $HOME/.bashrc
-
-echo "Exiting temporary user"
-exit
-
 echo "Installing programs with apt"
-echo <senha-wks> | sudo apt install hexedit nmap wireshark sleuthkit autopsy -y
-
-# Running apps that need UI configuration
-#./ghidra_11.3.1_PUBLIC/ghidraRun
-#chmod +x ./burp; ./burp
+echo <senha-wks> | sudo -S ls
+sudo apt install hexedit nmap wireshark sleuthkit autopsy sagemath -y
+sudo python3 -m pip install pycryptodome pwn Pillow
